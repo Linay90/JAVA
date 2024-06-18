7c4df1ffdfc4215bbc46c94652fd15334cb490f3
Linay90 <linaybharambe1108@gmail.com>
2024-06-18 20:39:54 +0530

    backend_template

---
+++ spring_boot_backend_template/src/main/java/com/app/dto/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.app.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+public class ApiResponse {
+	private String message;
+	private LocalDateTime timeStamp;
+	
+	public ApiResponse() {
+		
+	}
+
+	public ApiResponse(String message) {
+		super();
+		this.message = message;
+		this.timeStamp = LocalDateTime.now();
+	}
+	
+
+}
